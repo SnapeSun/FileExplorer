780a62720659c2877ed0197e441e2773d948d1ba
frank.zhang <frank.zhang@zhntd.com>
2016-10-13 12:28:35 +0800

    The Initial Commit

---
+++ app/src/main/java/com/zhntd/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.zhntd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by david on 16-10-10.
+ */
+
+public class SearchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
